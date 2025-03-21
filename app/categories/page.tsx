675aa7137962ff9900ba7ab2d8c7392165bedb6b
George <georgemarcellin00@gmail.com>
2025-03-21 20:59:42 +0700

    update category page

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,25 +1,28 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import CategoryCard from "../components/Category/CategoryCard";
 
 export interface Category {
   id: number;
-  imageUrl: string;
   categoryName: string;
   slug: string;
+}
+
+export interface CategoryRes {
+  categoryId: number;
+  name: string;
 }
 
 // Simulasi data produk
 const categories = [
   {
     id: 1,
-    imageUrl: "/placeholder.svg?height=300&width=400",
     categoryName: "Bouquet Bunga",
     slug: "bouquetbunga",
   },
   {
     id: 2,
-    imageUrl: "",
     categoryName: "Bouquet Uang",
     slug: "bouquetuang",
   },
@@ -49,7 +52,38 @@ const categories = [
   },
 ];
 
+
+  
 export default function CategoriesPage() {
+    const [categories, setCategories] = useState<Category[]>([]);
+   
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const res = await fetch("/api/category", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (!res.ok) throw new Error("Failed to fetch category");
+
+        const data = await res.json();
+        const transformedCategories = data.map((category : CategoryRes) => ({
+          id: category.categoryId,
+          categoryName: category.name,
+          slug: category.name.toLowerCase().replace(/\s+/g, ""),
+        }));
+        console.log(transformedCategories);
+
+        setCategories(transformedCategories); // ✅ Store actual data, not a promise
+        console.log(categories);
+      } catch (error) {
+        console.error("Error fetching category:", error);
+      }
+    };
+
+    fetchCategory();
+  }, []);
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8 text-center">Our Products</h1>
@@ -57,7 +91,7 @@ export default function CategoriesPage() {
         {categories.map((cat) => (
           <CategoryCard
             key={cat.id}
-            imageUrl={cat.imageUrl}
+            imageUrl={''}
             categoryName={cat.categoryName}
             slug={cat.slug}
           />
