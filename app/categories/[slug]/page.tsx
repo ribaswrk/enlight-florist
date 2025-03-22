e18838178423106ecb82cc226f05334593e67cd1
Ribas <richardribas1004@gmail.com>
2025-03-23 00:15:55 +0700

    fix warning and add format price

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -9,6 +9,10 @@ interface Product {
 	category: string;
 }
 
+import {
+	ArrowUturnLeftIcon
+} from "@heroicons/react/24/outline";
+
 // Fetch products from the API
 async function getProducts(slug: string): Promise<Product[]> {
 	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"; // ✅ Adjust for deployment
@@ -40,13 +44,21 @@ export default async function CategoryPage({
 
 	const products = await getProducts(slug);
 
+  const formatRupiah = (price: number) => {
+		return new Intl.NumberFormat("id-ID", {
+			style: "currency",
+			currency: "IDR",
+			minimumFractionDigits: 0,
+		}).format(price);
+	};
+
 	return (
 		<div className="container mx-auto px-4 py-8">
 			{/* Back Button */}
 			<div className="mb-4">
 				<Link href="/categories">
 					<button className="flex items-center text-black-500 hover:underline">
-						← Back to Categories
+						<ArrowUturnLeftIcon className="h-8 w-8 mr-2" />
 					</button>
 				</Link>
 			</div>
@@ -58,11 +70,8 @@ export default async function CategoryPage({
 					</h1>
 					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 						{products.map((product) => (
-							<Link href={"/productdetail"}>
-								<div
-									key={product.id}
-									className="border rounded-lg overflow-hidden shadow-md"
-								>
+							<Link href={"/productdetail"} key={product.id}>
+								<div className="border rounded-lg overflow-hidden shadow-md">
 									<div className="relative aspect-square">
 										<Image
 											src={product.image || "/placeholder.svg"}
@@ -74,7 +83,7 @@ export default async function CategoryPage({
 									</div>
 									<div className="p-4">
 										<h2 className="text-lg font-semibold">{product.name}</h2>
-										<p className="text-gray-600">${product.price}</p>
+										<p className="text-gray-600">{formatRupiah(product.price)}</p>
 									</div>
 								</div>
 							</Link>
