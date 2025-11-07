988503642bfffa21e10fbf38d6440b201fe7bfdf
George <georgemarcellin00@gmail.com>
2025-11-07 16:44:30 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 export const runtime = "nodejs";
+
 import NextAuth from "next-auth";
 import type { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
